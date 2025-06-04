168792fe04c9d0d6e782eff91ce18e154ab4b4bf
aza1fe <aleyraza.zaidi@etas.com>
2025-06-04 15:52:42 +0200

    bugfix2_decorators

---
+++ ooo/decorators/decorators1.py
@@ -0,0 +1,21 @@
+def hi(name="yasoob"):
+    return "hi " + name
+
+print(hi())
+# output: 'hi yasoob'
+
+# We can even assign a function to a variable like
+greet = hi
+# We are not using parentheses here because we are not calling the function hi
+# instead we are just putting it into the greet variable. Let's try to run this
+
+print(greet())
+# output: 'hi yasoob'
+
+# Let's see what happens if we delete the old hi function!
+#del hi  # if we activate del command, then it will delete hi variable. then next like print(hit()) will give an ERROR
+print(hi())
+#outputs: NameError
+
+print(greet())
+#outputs: 'hi yasoob'
